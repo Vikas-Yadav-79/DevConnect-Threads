91a44ed01badb43b7ec7596ba6962a15eeff74e2
Vikas Yadav <vikas1012.in@gmail.com>
2024-03-26 16:23:00 +0530

    Solved image not showing as reply bug:

--- DevConnect/fronted/src/components/Comments.jsx
+++ DevConnect/fronted/src/components/Comments.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Divider, Flex, Text ,Box,Image} from "@chakra-ui/react";
 
 const Comments = ({ reply, lastReply }) => {
 	return (
@@ -12,6 +12,16 @@ const Comments = ({ reply, lastReply }) => {
 						</Text>
 					</Flex>
 					<Text>{reply.text}</Text>
+                    {
+                        reply.img_rep && (
+                            <Box borderRadius={6} overflow={"hidden"} border={"1px solid red.500"}>
+                            <Image
+                                src={reply.img_rep} w={"full"}
+
+                            />
+                        </Box>
+                        )
+                    }
 				</Flex>
 			</Flex>
 			{!lastReply ? <Divider /> : null}
