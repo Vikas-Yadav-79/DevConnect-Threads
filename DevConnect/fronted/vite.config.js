dfca4432f41eebe952d3f3c38b7b9ad08dfb1958
Vikas Yadav <vikas1012.in@gmail.com>
2024-03-30 11:09:32 +0530

    modified Proxy

--- DevConnect/fronted/vite.config.js
+++ DevConnect/fronted/vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     port : 3000,
     proxy : {
       "/api" :{
-        target:"http://localhost:5000",
+        target:"https://dev-connect-api.vercel.app/",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       }
     }
   }
