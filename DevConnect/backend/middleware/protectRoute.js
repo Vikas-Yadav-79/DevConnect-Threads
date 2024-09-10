c47a872f77e1e50aca46804a06b07f208a223cc1
Vikas Yadav <vikas1012.in@gmail.com>
2024-09-10 14:49:54 +0530

    Solved Session and cookies bug

--- DevConnect/backend/middleware/protectRoute.js
+++ DevConnect/backend/middleware/protectRoute.js
@@ -1,35 +1,29 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
-const protectRoute = async (req,res,next) =>{
-
-    try{
-        const token = req.cookies.jwt;
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies && req.cookies.jwt;
 
         if (!token) {
-            return res.status(401).json({msg: "You are Not Logged in:"})
+            return res.status(401).json({ msg: "You are not logged in" });
         }
 
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         const user = await User.findById(decoded.userId).select("-password");
 
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
+        }
 
         req.user = user;
+
         next();
-
-
+    } catch (err) {
+        res.status(500).json({ message: "Server error: " + err.message });
+        console.log("Error occurred in protectRoute: " + err.message);
     }
-    catch(err){
+};
 
-        res.status(500).json({message: err.message});
-        console.log("Error Ocurred in protectRoute : "+ err.message)
-
-    }
-
-
-
-}
-
-
-module.exports = protectRoute
+module.exports = protectRoute;
